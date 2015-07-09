739fbb8483ccce946d71c67d480d9a1635742cec
Brigitte Bigi <brigitte.bigi@gmail.com>
2015-07-09 14:05:20 +0200

    site web slideshow seems ok

--- Packager/etc/scripts/slideshow.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-   $(function(){
-      setInterval(function(){
-         $(".slideshow ul").animate({marginLeft:-350},800,function(){
-            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-         })
-      }, 3000);
-   });
-</script>
